40c488c92cf545562fdae68c3a9e32346f336323
david <b00053364@gmail.com>
2015-12-14 14:57:42 +0000

    created Score Activity

--- app/src/main/java/com/oneilldavid/dartscoreboard/MainActivity.java
+++ app/src/main/java/com/oneilldavid/dartscoreboard/MainActivity.java
@@ -1,16 +1,31 @@
 package com.oneilldavid.dartscoreboard;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends ActionBarActivity {
+
+    ImageView play;
+    Context context = getApplicationContext();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        play = (ImageView) findViewById(R.id.play);
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context,ScoreActivity.class);
+            }
+        });
     }
 
     @Override
---
+++ app/src/main/java/com/oneilldavid/dartscoreboard/ScoreActivity.java
@@ -0,0 +1,17 @@
+package com.oneilldavid.dartscoreboard;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by david on 14/12/2015.
+ */
+public class ScoreActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score);
+    }
+
+}
